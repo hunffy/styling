da2e332436209d1148137767ae1bde838ca48b54
ksh <sehun910124@naver.com>
2023-10-31 20:47:07 +0900

    초기설정

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-const Main = () => {
+const Main = (): JSX.Element => {
   return (
-    <div>
-      <div>메인페이지</div>
+    <div className="MainWrapper">
+      <div className="header">
+        <div className="img_tag">이미지</div>
+        <div className="search_bar">검색바</div>
+        <div className="menu_bar">메뉴바</div>
+      </div>
     </div>
   );
 };
--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from "../components/header";
 import Footer from "../components/footer";
 import { Outlet } from "react-router-dom";
 const Root = () => {
   return (
     <div>
-      <Header />
       <main>
         <Outlet />
       </main>
