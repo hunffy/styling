04d762a6d5101f8b80cc84343d08ca113e223916
ksh <sehun910124@naver.com>
2023-10-31 21:09:47 +0900

    메인페이지 작업

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -5,7 +5,13 @@ const Main = (): JSX.Element => {
     <div className="MainWrapper">
       <div className="header">
         <div className="img_tag">이미지</div>
-        <div className="search_bar">검색바</div>
+        <div className="search_bar">
+          <div className="logo">로고</div>
+          <form action="get">
+            <input type="text" placeholder="검색어를 입력하세요" />
+            <button>검색</button>
+          </form>
+        </div>
         <div className="menu_bar">메뉴바</div>
       </div>
     </div>
